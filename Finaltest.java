ca3ed7f918d98e12ca2bfa5ba29c0e68a785b129
RushikeshDeshmukh <rushidesh700@gmail.com>
2018-10-26 11:20:48 +0530

    Add files via upload

---
+++ Finaltest.java
@@ -0,0 +1,69 @@
+package uiautomation.test;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import junit.framework.Test;
+import utility.ExcelUtils;
+ 
+@Test public class Finaltest {
+ 
+	private String sTestCaseName;
+ 
+	private int iTestCaseRow;
+ 
+	WebDriver driver;
+ 
+  @BeforeMethod
+ 
+  public void beforeMethod() throws Exception {
+ 
+	  WebDriver driver = new ChromeDriver(); // create object of ChromeDriver
+	  
+	  driver.manage().window().maximize();
+ 
+      driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+ 
+      driver.get("https://www.google.com/");	 
+ 
+  }	
+ 
+  @Test(dataProvider = "Authentication")
+ 
+  public void f(String Images, String Googlesearch) {
+ 
+	 //   driver.findElement(By.xpath(".//*[@id='account']/a")).click();
+ 
+	    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	    driver.findElement(By.xpath("//*[@id='lst-ib']")).sendKeys(Images);
+ 
+		System.out.println(Images);
+ 
+	    driver.findElement(By.id("//*[@id='tsf']/div[2]/div[3]/center/input[1]")).Click();
+ 
+		System.out.println(Googlesearch);
+  }
+ 
+
+ 
+  @DataProvider
+ 
+  public Object[][] Authentication() throws Exception{
+ 
+ 
+	 	ExcelUtils.setExcelFile("/DataDriven/src/Data/TestData.xlsx");
+ 
+	 	sTestCaseName = this.toString();
+ 
+	  	
+	  	sTestCaseName = ExcelUtils.getTestCaseName(this.toString());
+ 
+	
+	 	iTestCaseRow = ExcelUtils.getRowContains(sTestCaseName,0);
+ 
+	    Object[][] testObjArray = ExcelUtils.getTableArray("/DataDriven/src/Data/TestData.xlsx","Sheet1",iTestCaseRow);
+ 
+	    	return (testObjArray);
+ 
+		}
+ 
+}
